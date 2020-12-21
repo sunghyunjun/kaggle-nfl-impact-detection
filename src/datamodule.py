637f0d9cbda95e93723b8a9d80f446cfe6e35856
Sunghyun Jun <amiere.jun@gmail.com>
2020-12-21 22:30:24 +0900

    Add more image augmentation.

--- src/datamodule.py
+++ src/datamodule.py
@@ -141,7 +141,23 @@ class ImpactDataModule(pl.LightningDataModule):
     def get_train_transform(self):
         return A.Compose(
             [
+                A.OneOf(
+                    [
+                        A.HueSaturationValue(
+                            hue_shift_limit=0.2,
+                            sat_shift_limit=0.2,
+                            val_shift_limit=0.2,
+                            p=0.9,
+                        ),
+                        A.RandomBrightnessContrast(
+                            brightness_limit=0.2, contrast_limit=0.2, p=0.9
+                        ),
+                    ],
+                    p=0.9,
+                ),
+                A.ToGray(p=0.1),
                 A.HorizontalFlip(p=0.5),
+                A.VerticalFlip(p=0.5),
                 A.Resize(height=512, width=512, p=1.0),
                 ToTensorV2(p=1.0),
             ],
