b769c37873b888cdfcdab8cffb3b4bc32e06754a
Sunghyun Jun <amiere.jun@gmail.com>
2020-12-31 23:45:30 +0900

    Add overlap option. Fix variable's name.

--- src/prepare_label.py
+++ src/prepare_label.py
@@ -19,8 +19,19 @@ def load_obj(name):
         return pickle.load(f)
 
 
-def make_pickle_from_labels(data_dir="../dataset", debug=False, impact_only=False):
-    train_labels = pd.read_csv("../dataset/train_labels.csv").fillna(0)
+def make_overlap_array(overlap):
+    overlap_array_after = np.array(range(overlap)) + 1
+    overlap_array_before = overlap_array_after[::-1] * -1
+    overlap_array = np.concatenate([overlap_array_before, overlap_array_after], axis=0)
+    return overlap_array
+
+
+def make_pickle_from_labels(
+    dataset_dir="../dataset", debug=False, impactonly=False, overlap=None
+):
+    train_labels_path = os.path.join(dataset_dir, "train_labels.csv")
+    # train_labels = pd.read_csv("../dataset/train_labels.csv").fillna(0)
+    train_labels = pd.read_csv(train_labels_path).fillna(0)
 
     train_labels["impact"] = train_labels["impact"].astype("int64")
     train_labels["confidence"] = train_labels["confidence"].astype("int64")
@@ -33,10 +44,24 @@ def make_pickle_from_labels(data_dir="../dataset", debug=False, impact_only=Fals
     )
     train_labels.drop(train_labels[train_labels.frame == 0].index, inplace=True)
 
+    if overlap is not None:
+        overlap_array = make_overlap_array(overlap)
+        train_labels_with_impact = train_labels[train_labels["impact"] > 0]
+        pbar = tqdm(train_labels_with_impact[["video", "frame", "label"]].values)
+        for row in pbar:
+            pbar.set_description("Processing overlap array")
+            frames = overlap_array + row[1]
+            train_labels.loc[
+                (train_labels["video"] == row[0])
+                & (train_labels["frame"].isin(frames))
+                & (train_labels["label"] == row[2]),
+                "impact",
+            ] = 1
+
     train_labels["weight_0.1"] = np.where(train_labels["impact"] == 1, 1, 0.1)
     train_labels["weight_0.05"] = np.where(train_labels["impact"] == 1, 1, 0.05)
 
-    if impact_only:
+    if impactonly:
         train_labels = train_labels[train_labels.impact == 1]
 
     image_ids = train_labels.image.unique()
@@ -74,10 +99,14 @@ def make_pickle_from_labels(data_dir="../dataset", debug=False, impact_only=Fals
         ].values
         data[image_id] = boxes_labels
 
-    if impact_only:
-        filename = os.path.join(data_dir, "train_labels_impact_only")
-    else:
-        filename = os.path.join(data_dir, "train_labels")
+    filename = "train_labels"
+    if impactonly:
+        filename += "_impactonly"
+
+    if overlap is not None:
+        filename += "_overlap" + str(overlap)
+
+    filename = os.path.join(dataset_dir, filename)
     save_obj(data, filename)
 
 
@@ -87,9 +116,12 @@ def main():
     # parser.add_argument("--debug", type=str, default=False)
     parser.add_argument("--debug", action="store_true")
     parser.add_argument("--impactonly", action="store_true")
+    parser.add_argument("--overlap", type=int)
     args = parser.parse_args()
 
-    make_pickle_from_labels(args.dataset_dir, args.debug, args.impactonly)
+    args_dict = vars(args)
+    # make_pickle_from_labels(args.dataset_dir, args.debug, args.impactonly, args)
+    make_pickle_from_labels(**args_dict)
 
 
 if __name__ == "__main__":
