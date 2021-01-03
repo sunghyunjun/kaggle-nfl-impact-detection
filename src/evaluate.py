4277863b5b44abc66f977c90033561393f53f1a0
Sunghyun Jun <amiere.jun@gmail.com>
2021-01-04 08:23:20 +0900

    Add make_prediction_tta. Fix make_pred_filtered_df. Add make_pred_bothzone_filtered_df.

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,10 +2,14 @@ from argparse import ArgumentParser
 
 import copy
 import os
+import random
 
 import numpy as np
 import pandas as pd
 from scipy.optimize import linear_sum_assignment
+
+import torch
+from torchvision.ops.boxes import batched_nms, nms
 
 from tqdm import tqdm
 
@@ -163,6 +167,141 @@ def make_prediction(model, valid_loader, device, debug=False, fullsizeimage=Fals
     return image_ids, boxes_gt, labels_gt, boxes_pred, scores_pred, labels_pred
 
 
+def tensor_random_brightness_contrast(image, brightness_limit=0.2, contrast_limit=0.2):
+    alpha = 1.0 + random.uniform(contrast_limit, contrast_limit)
+    beta = 0.0 + random.uniform(brightness_limit, brightness_limit)
+
+    image *= alpha
+    image += beta * torch.mean(image)
+    image_clipped = torch.clip(image, 0, 1)
+
+    return image_clipped
+
+
+def make_prediction_tta(model, valid_loader, device, debug=False, fullsizeimage=False):
+    if fullsizeimage:
+        resize_height = 640
+        resize_width = 1280
+    else:
+        resize_height = 512
+        resize_width = 512
+
+    image_ids = []
+    boxes_gt = []
+    labels_gt = []
+    boxes_pred = []
+    scores_pred = []
+    labels_pred = []
+
+    for index, batch in enumerate(tqdm(valid_loader)):
+        if debug and index > 3:
+            break
+
+        image = batch["image"]
+        boxes_ = batch["bboxes"]
+        labels_ = batch["labels"]
+        image_id = batch["image_id"]
+
+        batch_size = len(image_id)
+
+        for i in range(batch_size):
+            boxes_[i] = boxes_[i][:, [1, 0, 3, 2]].numpy()
+
+            boxes_[i][:, 0] = boxes_[i][:, 0] * 1280 / resize_width
+            boxes_[i][:, 1] = boxes_[i][:, 1] * 720 / resize_height
+            boxes_[i][:, 2] = boxes_[i][:, 2] * 1280 / resize_width
+            boxes_[i][:, 3] = boxes_[i][:, 3] * 720 / resize_height
+
+            boxes_[i] = boxes_[i].astype(np.int32)
+
+            boxes_[i][:, 0] = boxes_[i][:, 0].clip(min=0, max=1280 - 1)
+            boxes_[i][:, 1] = boxes_[i][:, 1].clip(min=0, max=720 - 1)
+            boxes_[i][:, 2] = boxes_[i][:, 2].clip(min=0, max=1280 - 1)
+            boxes_[i][:, 3] = boxes_[i][:, 3].clip(min=0, max=720 - 1)
+
+            labels_[i] = labels_[i].numpy().astype(np.int32)
+
+        prediction = model(image.to(device))
+
+        # image_aug = image + torch.rand(image.shape) * 0.01
+        image_aug = tensor_random_brightness_contrast(image)
+
+        prediction_aug = model(image_aug.to(device))
+
+        boxes_org = prediction[:, :, :4]
+        scores_org = prediction[:, :, 4]
+        labels_org = prediction[:, :, 5]
+
+        boxes_aug = prediction_aug[:, :, :4]
+        scores_aug = prediction_aug[:, :, 4]
+        labels_aug = prediction_aug[:, :, 5]
+
+        boxes_cat = torch.cat((boxes_org, boxes_aug), axis=1)
+        scores_cat = torch.cat((scores_org, scores_aug), axis=1)
+        labels_cat = torch.cat((labels_org, labels_aug), axis=1)
+
+        boxes_nms = torch.zeros((batch_size, 100, 4))
+        scores_nms = torch.zeros((batch_size, 100))
+        labels_nms = torch.zeros((batch_size, 100))
+
+        for j in range(batch_size):
+            top_detection_idx = batched_nms(
+                boxes_cat[j, :, :],
+                scores_cat[j, :],
+                labels_cat[j, :],
+                iou_threshold=0.5,
+            )
+            # print(scores_cat[j, :].shape, labels_cat[j, :].shape)
+            # print(top_detection_idx.shape)
+            top_detection_idx = top_detection_idx[:100]
+            # print(top_detection_idx.shape)
+            # print(top_detection_idx)
+
+            boxes_nms[j, :, :] = torch.cat(
+                (
+                    boxes_cat[j, top_detection_idx],
+                    torch.zeros((100 - len(top_detection_idx), 4)),
+                ),
+            )
+            scores_nms[j, :] = torch.cat(
+                (
+                    scores_cat[j, top_detection_idx],
+                    torch.zeros((100 - len(top_detection_idx))),
+                )
+            )
+            labels_nms[j, :] = torch.cat(
+                (
+                    labels_cat[j, top_detection_idx],
+                    torch.zeros((100 - len(top_detection_idx))),
+                )
+            )
+
+        boxes = boxes_nms.detach().cpu().numpy()
+        scores = scores_nms.detach().cpu().numpy()
+        labels = labels_nms.detach().cpu().numpy().astype(np.int32)
+
+        boxes[:, :, 0] = boxes[:, :, 0] * 1280 / resize_width
+        boxes[:, :, 1] = boxes[:, :, 1] * 720 / resize_height
+        boxes[:, :, 2] = boxes[:, :, 2] * 1280 / resize_width
+        boxes[:, :, 3] = boxes[:, :, 3] * 720 / resize_height
+
+        boxes = boxes.astype(np.int32)
+
+        boxes[:, :, 0] = boxes[:, :, 0].clip(min=0, max=1280 - 1)
+        boxes[:, :, 1] = boxes[:, :, 1].clip(min=0, max=720 - 1)
+        boxes[:, :, 2] = boxes[:, :, 2].clip(min=0, max=1280 - 1)
+        boxes[:, :, 3] = boxes[:, :, 3].clip(min=0, max=720 - 1)
+
+        image_ids.extend(image_id)
+        boxes_gt.extend(boxes_)
+        labels_gt.extend(labels_)
+        boxes_pred.extend(boxes)
+        scores_pred.extend(scores)
+        labels_pred.extend(labels)
+
+    return image_ids, boxes_gt, labels_gt, boxes_pred, scores_pred, labels_pred
+
+
 def make_gt_df(image_ids, boxes_gt, labels_gt):
     boxes_gt_df = pd.DataFrame(
         np.concatenate(boxes_gt), columns=["xmin", "ymin", "xmax", "ymax"]
@@ -284,13 +423,44 @@ def make_pred_filtered_df(pred_df, iou_threshold=0.5):
             base_frame = base_row["frame"]
             for index, row in tmp_df.iterrows():
                 frame = row["frame"]
-                if frame > base_frame:
+                # if frame > base_frame:
+                if base_frame < frame <= base_frame + 4:
                     bbox = row[["xmin", "ymin", "xmax", "ymax"]].to_numpy()
                     iou_score = iou(base_bbox, bbox)
                     if iou_score > iou_threshold:
                         dropIDX.append(index)
                         # print(base_video, base_frame, frame, iou_score, len(set(dropIDX)))
     dropIDX = set(dropIDX)
+
+    pred_filtered_df = pred_df.drop(index=dropIDX).reset_index(drop=True)
+    return pred_filtered_df
+
+
+def make_pred_bothzone_filtered_df(pred_df):
+    dropIDX = []
+    for keys in pred_df.groupby(["gameKey", "playID"]).size().to_dict().keys():
+        tmp_df = pred_df.query("gameKey == @keys[0] and playID == @keys[1]")
+
+        for index, row in tmp_df.iterrows():
+            if row["view"] == "Endzone":
+                check_df = tmp_df.query('view == "Sideline"')
+                if (
+                    check_df["frame"]
+                    .apply(lambda x: np.abs(x - row["frame"]) <= 4)
+                    .sum()
+                    == 0
+                ):
+                    dropIDX.append(index)
+
+            if row["view"] == "Sideline":
+                check_df = tmp_df.query('view == "Endzone"')
+                if (
+                    check_df["frame"]
+                    .apply(lambda x: np.abs(x - row["frame"]) <= 4)
+                    .sum()
+                    == 0
+                ):
+                    dropIDX.append(index)
 
     pred_filtered_df = pred_df.drop(index=dropIDX).reset_index(drop=True)
     return pred_filtered_df
@@ -322,6 +492,7 @@ def main():
     parser.add_argument("--impactdefinitive", action="store_true")
     parser.add_argument("--seqmode", action="store_true")
     parser.add_argument("--fullsizeimage", action="store_true")
+    parser.add_argument("--tta", action="store_true")
     args = parser.parse_args()
 
     EXP_NAME = args.exp_name
@@ -336,6 +507,7 @@ def main():
     IMPACTDEFINITIVE = args.impactdefinitive
     SEQMODE = args.seqmode
     FULLSIZEIMAGE = args.fullsizeimage
+    TTA = args.tta
 
     if IMPACTONLY:
         IMPACT_CLASS = 1
@@ -394,16 +566,28 @@ def main():
     # ----------
     # evaluation
     # ----------
-    (
-        image_ids,
-        boxes_gt,
-        labels_gt,
-        boxes_pred,
-        scores_pred,
-        labels_pred,
-    ) = make_prediction(
-        impact_detector, dm.val_dataloader(), device, DEBUG, FULLSIZEIMAGE
-    )
+    if not TTA:
+        (
+            image_ids,
+            boxes_gt,
+            labels_gt,
+            boxes_pred,
+            scores_pred,
+            labels_pred,
+        ) = make_prediction(
+            impact_detector, dm.val_dataloader(), device, DEBUG, FULLSIZEIMAGE
+        )
+    else:
+        (
+            image_ids,
+            boxes_gt,
+            labels_gt,
+            boxes_pred,
+            scores_pred,
+            labels_pred,
+        ) = make_prediction_tta(
+            impact_detector, dm.val_dataloader(), device, DEBUG, FULLSIZEIMAGE
+        )
 
     gt_df = make_gt_df(image_ids, boxes_gt, labels_gt)
 
@@ -411,22 +595,44 @@ def main():
         image_ids, boxes_pred, scores_pred, labels_pred, IMPACT_CLASS
     )
 
-    SCORE_THRESHOLD = [0.30, 0.35, 0.40, 0.45, 0.50]
-    IOU_THRESHOLD = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    SCORE_THRESHOLD = [
+        0.26,
+        0.28,
+        0.30,
+        0.32,
+        0.34,
+        0.36,
+        0.38,
+        0.40,
+        0.42,
+        0.44,
+        0.46,
+        0.48,
+        0.50,
+    ]
+    IOU_THRESHOLD = [0.4, 0.5, 0.6, 0.7]
 
     for thres in SCORE_THRESHOLD:
         pred_df = pred_df_raw[pred_df_raw.score > thres]
-        print("-" * 110)
-        print(f"Raw - Score: {thres:.2f}             - ", end="")
+        print("-" * 120)
+        print(f"Raw - Score: {thres:.2f}             -    -", end="")
         tp, fp, fn, precision, recall, f1_score = calc_metric(gt_df, pred_df)
         if not DEBUG:
             neptune.log_metric(f"{thres:.2f}-RAW", f1_score)
 
         for iou_thres in IOU_THRESHOLD:
             pred_filtered_df = make_pred_filtered_df(pred_df, iou_thres)
-            print(f"PP  - Score: {thres:.2f} - iou: {iou_thres:.2f} - ", end="")
+            print(f"PP  - Score: {thres:.2f} - iou: {iou_thres:.2f} -    -", end="")
             tp, fp, fn, precision, recall, f1_score = calc_metric(
                 gt_df, pred_filtered_df
+            )
+            if not DEBUG:
+                neptune.log_metric(f"{thres:.2f}-{iou_thres:.2f}", f1_score)
+
+            pred_bothzone_filtered_df = make_pred_bothzone_filtered_df(pred_filtered_df)
+            print(f"PP  - Score: {thres:.2f} - iou: {iou_thres:.2f} - PP -", end="")
+            tp, fp, fn, precision, recall, f1_score = calc_metric(
+                gt_df, pred_bothzone_filtered_df
             )
             if not DEBUG:
                 neptune.log_metric(f"{thres:.2f}-{iou_thres:.2f}", f1_score)
